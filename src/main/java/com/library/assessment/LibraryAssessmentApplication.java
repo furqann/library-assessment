7d6f4070c9b7de34196015d4ec38ae2e4b8f8e27
Furqan Ahmad <furqanahmad21@hotmail.com>
2024-05-18 00:02:52 +0500

    Made spring to load context

--- src/main/java/com/library/assessment/LibraryAssessmentApplication.java
+++ src/main/java/com/library/assessment/LibraryAssessmentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryAssessmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryAssessmentApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LibraryAssessmentApplication.class, args);
+  }
 
 }
--- src/test/java/com/library/assessment/LibraryAssessmentApplicationTests.java
+++ src/test/java/com/library/assessment/LibraryAssessmentApplicationTests.java
@@ -2,7 +2,9 @@ package com.library.assessment;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class LibraryAssessmentApplicationTests {
 
