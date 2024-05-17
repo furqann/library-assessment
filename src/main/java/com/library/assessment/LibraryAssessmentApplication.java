876059c869e51407922c799362982d7efabd94d6
Mahnoor Gul <furqanahmad21@hotmail.com>
2024-05-17 11:59:16 +0500

    Using spring initializer generated spring boot project

---
+++ src/main/java/com/library/assessment/LibraryAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.library.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAssessmentApplication.class, args);
+	}
+
+}
